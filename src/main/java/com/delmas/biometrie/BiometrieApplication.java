056067daea9686387e03d8ed5a6d1a69971c01c2
delmas007 <azertyazerty1ze@gmail.com>
2025-02-24 09:59:10 +0000

    init

---
+++ src/main/java/com/delmas/biometrie/BiometrieApplication.java
@@ -0,0 +1,13 @@
+package com.delmas.biometrie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiometrieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BiometrieApplication.class, args);
+    }
+
+}
